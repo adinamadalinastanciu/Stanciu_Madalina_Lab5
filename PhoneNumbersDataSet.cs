cda06e07cf9144ddf4d1190da2ab72818a77d3f7
adinamadalinastanciu <stanciuserviciu@yahoo.com>
2020-11-08 22:32:26 +0200

    first commit

---
+++ PhoneNumbersDataSet.cs
@@ -0,0 +1,30 @@
+﻿namespace Stanciu_Madalina_Lab5
+{
+}
+
+namespace Stanciu_Madalina_Lab5
+{
+}
+
+namespace Stanciu_Madalina_Lab5
+{
+}
+
+namespace Stanciu_Madalina_Lab5
+{
+}
+
+namespace Stanciu_Madalina_Lab5
+{
+}
+
+namespace Stanciu_Madalina_Lab5
+{
+}
+
+namespace Stanciu_Madalina_Lab5
+{
+}
+
+public partial class PhoneNumbersDataSet {
+}
